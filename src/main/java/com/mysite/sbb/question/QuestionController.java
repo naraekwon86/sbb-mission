fbdd984c15a1a7b85da92c2a06a4a1a5747f689e
naraekwon86 <wde0530@gmail.com>
2023-12-04 13:50:59 +0900

    3-14

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -37,10 +37,12 @@ public class QuestionController {
     private final UserService userService;
 
     @GetMapping("/list")
-    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
-        Page<Question> paging = this.questionService.getList(page);
-        model.addAttribute("paging", paging);
-        return "question_list";
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+    @RequestParam(value = "kw" , defaultValue = "") String kw){
+            Page<Question> paging = this.questionService.getList(page, kw);
+            model.addAttribute("paging", paging);
+            model.addAttribute("kw", kw);
+            return "question_list";
     }
 
     @GetMapping(value = "/detail/{id}")
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -14,20 +14,45 @@ import lombok.RequiredArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.domain.Sort;
-
 //질문에 작성자 저장하기
 import com.mysite.sbb.user.SiteUser;
+//검색(Specification)
+import com.mysite.sbb.answer.Answer;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
     private final QuestionRepository  questionRepository;
 
-    public Page<Question> getList(int page){
+    private Specification<Question> search(String kw){
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb){
+                query.distinct(true); // 증복을 제거
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList",JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author",JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"),
+                        cb.like(q.get("content"), "%" + kw + "%"),
+                        cb.like(u1.get("username"), "%" + kw + "%"),
+                        cb.like(u2.get("content"), "%" + kw + "%"),
+                        cb.like(u2.get("username"), "%" + kw + "%"));
+            }
+        };
+    }
+    public Page<Question> getList(int page, String kw){
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page,10,Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+        return this.questionRepository.findAllByKeyword(kw,pageable);
     }
 
     public Question getQuestion (Integer id){
